6517b73dbafbc72c11037a574e3d390cc0add6cf
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-05-23 19:20:14 +0200

    Creating projects.jon

--- src/components/GridProjects/gridProjects.jsx
+++ src/components/GridProjects/gridProjects.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './style-gridProjects.css';
 import { Link } from "react-router-dom";
 import Project from '../project/project';
+import projects from './projects.json';
 
 export default function GridProjects() {
 
-    const projects = [
-        { title: 'Personal Web', subtitle: 'Made with HTML and CSS' },
-        { title: 'Saetabis Summer School', subtitle: 'Full developed, focusing on UX/UI' },
-        { title: 'Wod Maker', subtitle: 'Full developed, focusing on Front End.' },
-    ];
+    // const projects = [
+    //     { title: 'Personal Web', subtitle: 'Made with HTML and CSS' },
+    //     { title: 'Saetabis Summer School', subtitle: 'Full developed, focusing on UX/UI' },
+    //     { title: 'Wod Maker', subtitle: 'Full developed, focusing on Front End.' },
+    // ];
 
     return projects.map((project) => (
         <a href=""><div className="projects-card card-title">{project.title} <p>{project.subtitle}</p></div></a>
