1975535819a85b8df1f490a390c5c5a16ff9a9a1
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-05-23 19:41:58 +0200

    Update cover.jsx

--- src/components/project/projects-components/cover.jsx
+++ src/components/project/projects-components/cover.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import './style-cover.css';
 
-export default function Cover({ titulo, role, date }) {
+export default function Cover() {
 
     return <div>
         <div className="cover">
-            <h1>{titulo}</h1>
+            <h1>{project.title}</h1>
             <button><a href="">Visit Website</a></button>
         </div>
         <div className="note-cover">
-            <h2>{role}</h2>
-            <h2>{date}</h2>
+            <h2>{project.role}</h2>
+            <h2>{project.period}</h2>
         </div>
     </div>
 }
