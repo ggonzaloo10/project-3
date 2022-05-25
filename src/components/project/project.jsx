ed16fd93acef0fd2654b576868dbd4495e095b4a
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-05-25 21:12:04 +0200

    Update project.jsx

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import './style-project.css';
 import Cover from './projects-components/cover';
-import { useParams,useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import projects from '../GridProjects/projects.json';
+import { useParams } from 'react-router-dom';
 
 function Project() {
-  const [project, setProject] = useState();
+  const [project, setProject] = useState([]);
 
-  const parameters = useParams();
-  
-  function useEffect() {
+  const {parameters} = useParams();
 
-    setProject(projects.find(project => project.id === parameters.id));
+
+  // setProject(projects.find(project => {
+  //   return project.id === parameters.id;
+  //   console.log(project.id);
+  // }));
+
+  for (var i=0; i<projects.length; i++) {
+    if(projects[i].id === parameters.id) {
+      setProject(projects[i]);
+      console.log(projects[i].id);
+    }
   }
+
 
   return (<>
     <h1>hola</h1>
     <div className="project-page">
       <h1>{project && project.title}</h1>
-      <Cover title={project.title}/>
+      <Cover title={project.title} />
       {/*
       <Intro/>
       <Content/>
       <FinalResult/>
       <Moreprojects/> */}
     </div>
-    </>
+  </>
   );
 }
 
