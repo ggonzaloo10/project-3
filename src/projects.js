c3f19dae8841754f73ac021c8fae27c90e7c0fbe
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-05-30 17:20:20 +0200

    emoji-book added to projects

--- src/projects.js
+++ src/projects.js
@@ -29,6 +29,16 @@ let projects = [
         introduction: "Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis, lacinia nascetur velit ultrices nec diam tempus consequat magnis, lectus etiam dictumst vestibulum torquent hac lobortis. Suspendisse lobortis dictumst nisi tincidunt inceptos, turpis tellus blandit interdum sollicitudin, pellentesque ut himenaeos natoque. Conubia velit porttitor diam vitae interdum facilisi, quam ut habitant at cum turpis, hac sem facilisis nibh rhoncus.",
         analysis: "Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis",
         finalresult: "Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis"
+    },
+
+    {
+        id: "emoji-book",
+        title: "Emoji Book",
+        subtitle: "Developed with php and Wordpress.",
+        period: "2022",
+        introduction: "Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis, lacinia nascetur velit ultrices nec diam tempus consequat magnis, lectus etiam dictumst vestibulum torquent hac lobortis. Suspendisse lobortis dictumst nisi tincidunt inceptos, turpis tellus blandit interdum sollicitudin, pellentesque ut himenaeos natoque. Conubia velit porttitor diam vitae interdum facilisi, quam ut habitant at cum turpis, hac sem facilisis nibh rhoncus.",
+        analysis: "Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis",
+        finalresult: "Lorem ipsum dolor sit amet consectetur adipiscing elit venenatis"
     }
 ]
 
