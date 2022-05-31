53a3c4622e9c1e69bf0c91a046fc93b05b3bef3b
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-05-31 15:08:08 +0200

    Update gridProjects.jsx

--- src/components/GridProjects/gridProjects.jsx
+++ src/components/GridProjects/gridProjects.jsx
@@ -3,21 +3,22 @@ import { getAllProjects } from "../../projects";
 import { Link } from 'react-router-dom';
 
 export default function GridProjects() {
+    
     const projects = getAllProjects();
 
 
     return (<div className='projects-container grid-projects'>
+
         {projects.map(project =>
+
             <div key={project.id} className="projects-card card-title">
+
                 <Link to={"/projects/" + project.id} className="link-card">
                     {project.title}
                     <p>{project.subtitle}</p>
                 </Link>
-            </div>)}
-        {/* <Link to={"/projects/" + props.id}>{props.title}
-<p>{props.subtitle}</p></Link> */}
 
-    </div>
-    )
+            </div>)}
+    </div>)
 }
 
