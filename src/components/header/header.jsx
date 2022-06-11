04446ff251559b9772dc94602de3713e8b5403da
ggonzaloo10 <92878148+ggonzaloo10@users.noreply.github.com>
2022-06-11 18:16:29 +0200

    update header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,20 +11,20 @@ export default function Header() {
         setStateMenu(prevValue => !prevValue);
     };
 
-    return <header>
+    return <header className="header header-desktop">
 
         <div onClick={onOff} className="menu-icon">
-        { stateMenu ?  
-            <svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line x1="1.93934" y1="35.8805" x2="35.8805" y2="1.93938" stroke="#CDC392" stroke-width="3" />
-                <line x1="4.06066" y1="1.93934" x2="38.0018" y2="35.8805" stroke="#CDC392" stroke-width="3" />
-            </svg> : 
-            <svg width="48" height="31" viewBox="0 0 48 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line y1="1.5" x2="48" y2="1.5" stroke="#CDC392" stroke-width="3" />
-            <line x1="16" y1="15.5" x2="48" y2="15.5" stroke="#CDC392" stroke-width="3" />
-            <line x1="28" y1="29.5" x2="48" y2="29.5" stroke="#CDC392" stroke-width="3" />
-        </svg> 
-        }
+            {stateMenu ?
+                <svg width="40" height="37" viewBox="0 0 40 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="1.93934" y1="35.8805" x2="35.8805" y2="1.93938" stroke="#CDC392" stroke-width="3" />
+                    <line x1="4.06066" y1="1.93934" x2="38.0018" y2="35.8805" stroke="#CDC392" stroke-width="3" />
+                </svg> :
+                <svg width="48" height="31" viewBox="0 0 48 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line y1="1.5" x2="48" y2="1.5" stroke="#CDC392" stroke-width="3" />
+                    <line x1="16" y1="15.5" x2="48" y2="15.5" stroke="#CDC392" stroke-width="3" />
+                    <line x1="28" y1="29.5" x2="48" y2="29.5" stroke="#CDC392" stroke-width="3" />
+                </svg>
+            }
         </div>
         {stateMenu && <div className="menu-links">
             <Link to={"/home"}>Home</Link>
@@ -32,5 +32,12 @@ export default function Header() {
             <Link to={"/projects"}>Projects</Link>
             <a href="#contact">Contact</a>
         </div>}
+
+        <div className="menu-links_desktop">
+            <Link to={"/home"}>Home</Link>
+            <Link to={"/about-me"}>About me</Link>
+            <Link to={"/projects"}>Projects</Link>
+            <a href="#contact">Contact</a>
+        </div>
     </header>;
 }
